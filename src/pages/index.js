157c7d5512ee191b15c3d14308ff9afeebfc3f72
wassem7 <waseemdarkwa@gmail.com>
2023-05-10 18:59:13 +0100

    initilization

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
+
+      {/* ---- ---- */}
+    </div>
+  );
+}
