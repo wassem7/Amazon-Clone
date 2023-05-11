eb5461cc511ee7d6527ab30197b90c3a38b31520
wassem7 <waseemdarkwa@gmail.com>
2023-05-11 11:09:26 +0100

    Top Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
 const Header = () => {
   return (
     <header>
       {/* Top nav */}
       <div className='bg-amazon_blue p-1 flex items-center py-2 flex-grow'>
         {/* logo */}
-        <div className='bg-red-400 flex items-center mt-2 flex-grow sm:flex-grow-0'>
+        <div className='flex items-center mt-2 flex-grow sm:flex-grow-0'>
           <Image
             width={150}
             height={40}
@@ -14,6 +19,37 @@ const Header = () => {
             objectFit='contain'
             className='cursor-pointer'
           />
+        </div>
+
+        {/* Search bar */}
+        <div className='bg-yellow-400 hover:bg-yellow-500 hidden sm:flex items-center rounded-md flex-grow hover:cursor-pointer h-10'>
+          <input
+            type='text'
+            className='h-full w-6 rounded-l-md flex flex-grow flex-shrink focus:outline-none px-4'
+          />
+          <SearchIcon className='h-12 p-4' />
+        </div>
+        {/* Right */}
+        <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+          <div className='link'>
+            <p>Hello Wassem Darkwa</p>
+            <p className='font-extrabold md:text-sm'>
+              Accounts & List
+            </p>
+          </div>
+          <div className='link'>
+            <p>Returns</p>
+            <p className='font-extrabold md:text-sm'>& orders</p>
+          </div>
+          <div className='relative link flex items-center '>
+            <span className='absolute top-0 right-0 md:right-10 bg-yellow-400 h-4 w-4 rounded-full text-black font-bold text-center'>
+              0
+            </span>
+            <ShoppingCartIcon className='h-10' />
+            <p className='font-extrabold md:text-sm hidden md:inline mt-2'>
+              Basket
+            </p>
+          </div>
         </div>
       </div>
 
