788f895d6376d371008afd28ca017b462cde5257
wassem7 <waseemdarkwa@gmail.com>
2023-05-11 22:28:32 +0100

    Carousel component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Banner from '../components/Banner';
+import ProductFeed from '../components/ProductFeed';
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-gray-100'>
       <Head>
         <title>Amazon 2.0</title>
       </Head>
 
       {/* Header */}
       <Header />
+
+      <main className=' max-w-screen-xl mx-auto'>
+        {/* Banner */}
+        <Banner />
+        {/* Product feed */}
+        <ProductFeed />
+      </main>
     </div>
   );
 }
