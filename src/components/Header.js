924ee82e1f7dbf68a193cfdb3bf297beaaf0ca68
wassem7 <waseemdarkwa@gmail.com>
2023-05-11 11:29:26 +0100

    Bottom navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -54,7 +54,27 @@ const Header = () => {
       </div>
 
       {/* Bottom Nav */}
-      <div></div>
+      <div className='flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6'>
+        <p className='flex items-center link'>
+          <MenuIcon className='h-6 mr-1' />
+          All
+        </p>
+        <p className='link'>Prime Video</p>
+        <p className='link'>Amazon Video</p>
+        <p className='link'>Today's deals</p>
+        <p className='link hidden lg:inline-flex'>Electronics</p>
+        <p className='link hidden lg:inline-flex'>
+          Food & Grocery
+        </p>
+        <p className='link hidden lg:inline-flex'>Prime</p>
+        <p className='link hidden lg:inline-flex'>Buy again</p>
+        <p className='link hidden lg:inline-flex'>
+          Shopper Toolkit
+        </p>
+        <p className='link hidden lg:inline-flex'>
+          Health & Personal Care
+        </p>
+      </div>
     </header>
   );
 };
